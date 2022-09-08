0d5c89a2ae011fe3aabe7a208d1412cdd1a89ff6
AishaKhalfan <aishakhalfan45@gmail.com>
2022-09-08 22:25:01 +0000

    Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.Use the function printf;Use the function printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**This is the
+ * preffered method
+ * for multiline commnets 
+ * This is the main
+ * function
+ * also
+ */
+int main ()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
+
+
