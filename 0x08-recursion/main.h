1f9fc42700b578f4583d0837f8029416121e8cdf
AishaKhalfan <aishakhalfan45@gmail.com>
2022-09-29 10:17:22 +0000

    a function that returns 1 if the input integer is a prime number, otherwise return 0.

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int natural_square(int x, int y);
 int is_prime_number(int n);
+int pnumber(int x, int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
