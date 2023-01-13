64a4bbaa04dfdeadbf243abc800cb308beb07489
AishaKhalfan <aishakhalfan45@gmail.com>
2023-01-13 14:59:08 +0000

    0x13. C - More singly linked lists:TASKS-10,11,12,13 and 14

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,5 +7,31 @@
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *previous_ptr;
+	listint_t *next_ptr;
+	unsigned int i;
 
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	next_ptr = *head;
+	previous_ptr = NULL;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(next_ptr);
+		return (1);
+	}
+	for (i = 0; i < index; i += 1)
+	{
+		if (next_ptr == NULL)
+			return (-1);
+		previous_ptr = next_ptr;
+		next_ptr = next_ptr->next;
+	}
+	if (previous_ptr)
+		previous_ptr->next = next_ptr->next;
+	free(next_ptr);
+
+	return (1);	
 }
